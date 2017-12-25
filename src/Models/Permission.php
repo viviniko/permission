6309a5e4c710e6ed91eabc3da5a42c6dc8132ce4
viviniko <viviniko.shop@gmail.com>
2017-12-25 09:17:06 +0800

    permission table name

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -2,9 +2,21 @@
 
 namespace Viviniko\Permission\Models;
 
+use Illuminate\Support\Facades\Config;
 use Zizaco\Entrust\EntrustPermission;
 
 class Permission extends EntrustPermission
 {
     protected $fillable = ['name', 'display_name', 'description'];
+
+    /**
+     * Creates a new instance of the model.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = Config::get('permission.permissions_table');
+    }
 }
--- src/Models/Role.php
+++ src/Models/Role.php
@@ -2,9 +2,21 @@
 
 namespace Viviniko\Permission\Models;
 
+use Illuminate\Support\Facades\Config;
 use Zizaco\Entrust\EntrustRole;
 
 class Role extends EntrustRole
 {
     protected $fillable = ['name', 'display_name', 'description'];
+
+    /**
+     * Creates a new instance of the model.
+     *
+     * @param array $attributes
+     */
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = Config::get('permission.roles_table');
+    }
 }
